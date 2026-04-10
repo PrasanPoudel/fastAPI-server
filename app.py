b5253a56c769c1e66026aba32b189d08abc13c9f
PrasanPoudel <prasanpoudel501@gmail.com>
2026-04-10 22:49:59 +0545

    .

--- app.py
+++ app.py
@@ -129,9 +129,9 @@ async def predict_fraud(job_data: JobData):
         if p >= 0.5:
             fraud_score = 1
         elif p >= 0.25:
-                fraud_score = 0.5 + p  
+                fraud_score = p * 2
         else:
-            fraud_score=p*2
+            fraud_score= p
         
         return {
             "fraudScore": fraud_score,
